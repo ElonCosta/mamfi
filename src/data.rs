c9c5870911ecd3a88122c9d6f5c489f9221785e1
ElonCosta <santos.elon@gmail.com>
2026-09-22 17:26:23 -0300

    feat: Migrate to new binary data converter

--- src/data.rs
+++ src/data.rs
@@ -1,84 +1,90 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{BufReader, BufWriter},
+    path::PathBuf,
 };
 
-use crate::{AppResult, FILE_NAME, Success};
+use crate::{
+    AppResult, FILE_NAME, Success,
+    cli::CommandOptions,
+    binary_data::{BinaryReader, BinaryType, BinaryWritter},
+    result,
+};
 pub struct FileData {
+    pub root_dir: PathBuf,
     pub data: HashMap<String, String>,
     pub set_file: Option<String>,
+    pub as_sym_link: bool,
+    pub silent: bool,
 }
 
-pub fn load_file_data() -> FileData {
+impl FileData {
+    pub fn is_silent(&self, command_options: &CommandOptions) -> bool {
+        (self.silent && !command_options.verbose()) || command_options.silent()
+    }
+}
+
+pub fn load_file_data(root_dir: PathBuf) -> result::Result<FileData> {
     let mut set_file: Option<String> = None;
     let mut data = HashMap::new();
+    let mut is_sym_link: Option<bool> = None;
+    let mut is_silent: Option<bool> = None;
 
-    let file = File::open(FILE_NAME);
+    let file = File::open(root_dir.join(FILE_NAME));
 
     if let Ok(file) = file {
         let reader = BufReader::new(file);
 
-        for line in reader.lines() {
-            let Ok(line) = line else {
-                continue;
-            };
-
-            let bytes = line.as_bytes();
-
-            let Some(i) = bytes.first() else {
-                continue;
-            };
-
-            let bytes = &bytes[1..];
-
-            match i {
-                b'1' => {
-                    let file = line[1..].trim();
-
-                    if file.is_empty() {
-                        continue;
-                    };
-
-                    set_file = Some(file.to_string());
+        for line in reader.binary_data() {
+            match line {
+                BinaryType::SetFile(file) => set_file = Some(file),
+                BinaryType::AliasEntry((alias, path)) => {
+                    data.insert(alias, path);
                 }
-                b'2' => {
-                    let key_size = bytes[0] as usize;
-                    let line = &line[2..];
-
-                    let alias = &line[..key_size].trim();
-                    let path = &line[key_size..].trim();
-
-                    data.insert(alias.to_string(), path.to_string());
+                BinaryType::Flags { sym_link, silent } => {
+                    is_sym_link = Some(sym_link);
+                    is_silent = Some(silent);
                 }
-                _ => continue,
             }
         }
     }
 
-    FileData { data, set_file }
+    Ok(FileData {
+        root_dir,
+        data,
+        set_file,
+        as_sym_link: is_sym_link.unwrap_or(false),
+        silent: is_silent.unwrap_or(false),
+    })
 }
 
-pub fn save_file_data(file_data: FileData) -> AppResult {
-    let FileData { data, set_file } = file_data;
+pub fn save_file_data(app_data: FileData) -> AppResult {
+    let FileData {
+        root_dir,
+        data,
+        set_file,
+        as_sym_link,
+        silent,
+    } = app_data;
 
-    let file = File::create(FILE_NAME)?;
+    let file = File::create(root_dir.join(FILE_NAME))?;
 
     let mut writer = BufWriter::new(file);
 
     if let Some(set_file) = set_file {
-        writer.write_all(b"1")?;
-        writer.write_all(set_file.as_bytes())?;
-        writer.write_all(b"\n")?;
+        writer.write_bin(BinaryType::SetFile(set_file))?;
     }
 
-    for (key, path) in data.iter() {
-        let key_size = key.len() as u8;
+    if as_sym_link || silent {
+        writer.write_bin(BinaryType::Flags {
+            sym_link: as_sym_link,
+            silent,
+        })?;
+    }
 
-        writer.write_all(&[b'2', key_size])?;
-        writer.write_all(key.as_bytes())?;
-        writer.write_all(path.as_bytes())?;
-        writer.write_all(b"\n")?;
+    for entry in data.into_iter() {
+        writer.write_bin(BinaryType::AliasEntry(entry))?;
     }
 
     Ok(Success::DataSaved)
